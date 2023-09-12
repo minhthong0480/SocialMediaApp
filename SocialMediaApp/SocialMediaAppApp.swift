35154dab5c8fe9be5706148d12e03b1a29e4b546
Minh Thong <s3852882@rmit.edu.vn>
2023-09-12 14:27:34 +0700

    Initial Commit

---
+++ SocialMediaApp/SocialMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaAppApp.swift
+//  SocialMediaApp
+//
+//  Created by Nguyen Hoang Minh Thong on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
