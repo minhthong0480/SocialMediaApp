28129e382b06b28acb293241683490d5f05427a7
Minh Thong <s3852882@rmit.edu.vn>
2023-09-13 17:13:17 +0700

    add profile view

---
+++ SocialMediaApp/Core/Profile/Views/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  SocialMediaApp
+//
+//  Created by Nguyen Hoang Minh Thong on 13/09/2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Paceholder for profile page")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
