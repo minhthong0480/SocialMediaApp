d8b4828ef70811f7d443b322068cd92030e926af
Minh Thong <s3852882@rmit.edu.vn>
2023-09-13 15:00:25 +0700

    change folder architecture

---
+++ SocialMediaApp/Core/Authentication/Views/SignupViews.swift
@@ -0,0 +1,20 @@
+//
+//  SignupViews.swift
+//  SocialMediaApp
+//
+//  Created by Nguyen Hoang Minh Thong on 13/09/2023.
+//
+
+import SwiftUI
+
+struct SignupViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignupViews_Previews: PreviewProvider {
+    static var previews: some View {
+        SignupViews()
+    }
+}
