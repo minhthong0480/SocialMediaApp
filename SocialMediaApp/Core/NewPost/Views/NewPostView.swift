65102bea218c09180e285fcd003943e237815666
kietduong23 <duonganhkiet2306@gmail.com>
2023-09-15 00:23:05 +0700

    Design NewPostView

--- SocialMediaApp/Core/NewPost/Views/NewPostView.swift
+++ SocialMediaApp/Core/NewPost/Views/NewPostView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct NewPostView: View {
+    @State private var caption = ""
     var body: some View {
-        Text("This is where user can create new post")
+        VStack() {
+            VStack(spacing: 10){
+                Image(systemName: "person")
+                    .resizable()
+                //                .scaledToFill()
+                    .clipShape(Circle())
+                    .frame(width:150, height:150)
+                //                .border(.black)
+                VStack(){
+                    Text("John Doe")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                    Text("@johndoe")
+                        .font(.caption)
+                }
+            }
+            TextEditor(text: $caption)
+                .foregroundStyle(.primary)
+                .padding()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(.black, lineWidth: 1)
+                }
+            Divider()
+            HStack(spacing:15) {
+                Spacer()
+                Button {
+                    
+                } label: {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.black)
+                        .overlay {
+                            HStack {
+                                Text("Post")
+                                    .foregroundColor(.white)
+                            }
+                        }
+                        .frame(width: 100, height: 40)
+                }
+            }
+            .frame(height:40)
+            Spacer()
+        } //VStack
+        .padding()
+        //        .frame(width: UIScreen.main.bounds.width - 40)
     }
+    
 }
 
 struct NewPostView_Previews: PreviewProvider {
