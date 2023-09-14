255a0173ab319e0f6cb762ac0d8407b4b56c2000
kietduong23 <duonganhkiet2306@gmail.com>
2023-09-14 23:27:04 +0700

    Create Feed which includes posts

--- SocialMediaApp/Core/Feed/ViewModels/FeedViewModel.swift
+++ SocialMediaApp/Core/Feed/ViewModels/FeedViewModel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class FeedViewModel: ObservableObject {
+    @Published var posts = [Post]()
+    
+    init() {
+        getPosts()
+    }
+    
+    func getPosts() {
+        self.posts = [Post(caption: "Example 1"), Post(caption: "Example 2"), Post(caption: "Example 3")]
+    }
+}
--- SocialMediaApp/Core/Feed/Views/FeedView.swift
+++ SocialMediaApp/Core/Feed/Views/FeedView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct FeedView: View {
+    @ObservedObject var viewModel = FeedViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ForEach(viewModel.posts) { post in
+                PostView(post: post)
+            }
+        }
     }
 }
 
