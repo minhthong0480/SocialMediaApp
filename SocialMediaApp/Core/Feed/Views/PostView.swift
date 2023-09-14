012cfcf259cd95116fcb6385c8064dc601ad9f12
vinhhungnh <s3911246@rmit.edu.vn>
2023-09-14 20:04:25 +0700

    add post view

--- SocialMediaApp/Core/Feed/Views/PostView.swift
+++ SocialMediaApp/Core/Feed/Views/PostView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct PostView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack(alignment:.leading){
+            HStack(alignment:.top, spacing: 10){
+                Image(systemName: "person")
+                    .resizable()
+                    //.scaledToFill()
+                    //.clipShape(Circle())
+                    .frame(width:45, height:45)
+                    //.border(.black)
+                VStack(alignment:.leading){
+                    Text("John Doe")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                    Text("@johndoe")
+                        .font(.caption)
+                }
+                Spacer()
+                Text("Just now")
+                    .font(.caption)
+            }
+            Text("This is an example caption.")
+            Rectangle()
+                .frame(height:400)
+                .cornerRadius(20)
+        }
+        .frame(width: UIScreen.main.bounds.width - 40)
     }
 }
 
