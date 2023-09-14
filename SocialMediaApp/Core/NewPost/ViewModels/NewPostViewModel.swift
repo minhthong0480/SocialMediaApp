9b8d0692fd3cee2b66f9f3484fbb7391ea99bfe8
kietduong23 <duonganhkiet2306@gmail.com>
2023-09-14 23:27:26 +0700

    Create NewPostView

---
+++ SocialMediaApp/Core/NewPost/ViewModels/NewPostViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewPostViewModel.swift
+//  SocialMediaApp
+//
+//  Created by Duong Anh Kiet on 14/09/2023.
+//
+
+import Foundation
---
+++ SocialMediaApp/Core/NewPost/Views/NewPostView.swift
@@ -0,0 +1,20 @@
+//
+//  NewPostView.swift
+//  SocialMediaApp
+//
+//  Created by Duong Anh Kiet on 14/09/2023.
+//
+
+import SwiftUI
+
+struct NewPostView: View {
+    var body: some View {
+        Text("This is where user can create new post")
+    }
+}
+
+struct NewPostView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewPostView()
+    }
+}
