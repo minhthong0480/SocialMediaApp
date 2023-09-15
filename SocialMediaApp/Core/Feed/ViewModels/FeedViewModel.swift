9374c6e7428968dfd282064766832fc914795d67
duonganhkiet23 <duonganhkiet2306@gmail.com>
2023-09-15 12:19:39 +0700

    Create api to fetch posts

--- SocialMediaApp/Core/Feed/ViewModels/FeedViewModel.swift
+++ SocialMediaApp/Core/Feed/ViewModels/FeedViewModel.swift
@@ -6,15 +6,31 @@
 //
 
 import Foundation
+import FirebaseFirestore
 
 class FeedViewModel: ObservableObject {
     @Published var posts = [Post]()
-    
+    private var db = Firestore.firestore()
     init() {
-        getPosts()
+        getAllPosts()
     }
     
-    func getPosts() {
-        self.posts = [Post(caption: "Example 1"), Post(caption: "Example 2"), Post(caption: "Example 3")]
+    func getAllPosts() {
+        //        self.posts = [Post(caption: "Example 1"), Post(caption: "Example 2"), Post(caption: "Example 3")]
+        
+        // Retrieve the "posts" document
+        db.collection("posts").addSnapshotListener { (querySnapshot, error) in
+            guard let documents = querySnapshot?.documents else {
+                print("No documents")
+                return
+            }
+            
+            // Loop to get the "name" field inside each movie document
+            self.posts = documents.map { (queryDocumentSnapshot) -> Post in
+                let data = queryDocumentSnapshot.data()
+                let caption = data["caption"] as? String ?? ""
+                return Post(caption: caption)
+            }
+        }
     }
 }
