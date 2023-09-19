d5e4da62b61f98f5e2e0af170cf0d7a0845bc3f5
kietduong23 <duonganhkiet2306@gmail.com>
2023-09-19 12:27:58 +0700

    Add NewPost

---
+++ SocialMediaApp/Core/NewPost/ViewModels/NewPostViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  NewPostViewModel.swift
+//  SocialMediaApp
+//
+//  Created by Duong Anh Kiet on 14/09/2023.
+//
+
+import Foundation
+import FirebaseFirestore
+import Firebase
+
+class NewPostViewModel: ObservableObject {
+    private let postService = PostService()
+    
+    func addNewPost (caption: String) {
+        postService.addNewPost(caption: caption)
+    }
+}
+
+
+
+
